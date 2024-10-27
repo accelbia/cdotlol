cd602eedc302573fcc0f1d93d49be545d00e309e
Ayush <abex.official.cert@gmail.com>
2024-10-28 05:20:41 +0530

    Changed contents

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className='container'>
-      <h1>Keep Aaradhya till he gets married, thanks. </h1>
-      And give me a referral if possible, hehe.
+    <div className="container">
+    Hehehe, wrong website sike!
     </div>
+    
     </>
   );
 }
